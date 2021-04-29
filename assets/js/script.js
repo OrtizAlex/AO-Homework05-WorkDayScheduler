9589ace9fa0f349b0a2d47f68419d9d9b66a608f
OrtizAlex <46968556+OrtizAlex@users.noreply.github.com>
2021-04-29 12:20:55 -0400

    Changed time block id to military time. Added checkTimeBlock function to color time block based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,38 @@
 //Set current time to header
-var timeEl = document.getElementById("currentDay");
+var timeEl = $("#currentDay");
 var currentTime = moment();
-timeEl.textContent = currentTime;
+timeEl.text(currentTime);
+//Color time block from start
+checkTimeBlock();
+
 //Set interval to constanstly update time
-var clock = setInterval(function(){
+var clock = setInterval(clockUpdater, 1000)
+
+function clockUpdater(){
     currentTime = moment();
-    timeEl.textContent = currentTime;
-},1000)
+    timeEl.text(currentTime);
+    checkTimeBlock();
+}
+
+function checkTimeBlock(){
+    var currentHour = currentTime.hours();
+    var timeBlock = $(".time-block");
+    for(var i = 0; i < timeBlock.length; i++){
+        var block = timeBlock[i];
+        if(parseInt(block.id.split("-")[0]) < currentHour){
+            $(block).addClass("past");
+        }
+        else if(parseInt(block.id.split("-")[0]) === currentHour){
+            $(block).removeClass("past");
+            $(block).addClass("present");
+        }
+        else{
+            $(block).removeClass("past");
+            $(block).removeClass("present");
+            $(block).addClass("future");
+        }
+    }
+}
 
 
 
